007b7e6115e28f1af9645d353fc5db38cdc373dd
Russell McGregor <rdmcgregor@gmail.com>
2017-11-11 14:04:07 +1000

    Ammend code and docs to support both SNMP v2 (for windows) and v3

--- README.md
+++ README.md
@@ -1,46 +1,60 @@
-Solarwinds/Puppet Custom Function
-=================================
+# Solarwinds/Puppet Custom Function
 
 This is a Puppet custom function for adding nodes to Solarwinds automagically
 
-Every puppet run the function will reach out to your Solarwinds instance and ask if it is already being monitored.  If it finds out that it does not exist it will add itself.
+Every puppet run the function will reach out to your Solarwinds instance and ask if it is already being monitored. If it finds out that it does not exist it will add itself.
 
+This is a hybrid version that supports both SNMP v2 and v3. Thanks to powellnathanj for the SNMP v2 version <https://github.com/powellnathanj/solarwinds_functions> Thanks to ggrosnick for the SNMP v3 version <https://github.com/ggrosnick/solarwinds_functions>
 
-Thanks to powellnathanj for the SNMP v2 version https://github.com/powellnathanj/solarwinds_functions
-
-Installation
-===========
+# Installation
 
 Add this repository to the modules directory of your puppet environment.
 
 Then add the following to your hieradata (common.yaml or other)
 
 A username/password that has authorization to update Solarwinds via the Orion API
-    
-    solarwinds_functions::config::username: ""
-    solarwinds_functions::config::password: ""
 
-The url you'd like to use for querying (ex: https://sw.yourdomain.com:17778/SolarWinds/InformationService/v3/Json/Query)
-    
-    solarwinds_functions::config::queryurl: ""
+```
+solarwinds_functions::config::username: ""
+solarwinds_functions::config::password: ""
+```
 
-The url you'd like to use for Creating (ex: https://sw.yourdomain.com:17778/SolarWinds/InformationService/v3/Json/Create/Orion.Nodes)
-    
-    solarwinds_functions::config::addurl: ""
+The url you'd like to use for querying (ex: <https://sw.yourdomain.com:17778/SolarWinds/InformationService/v3/Json/Query>)
+
+```
+solarwinds_functions::config::queryurl: ""
+```
+
+The url you'd like to use for Creating (ex: <https://sw.yourdomain.com:17778/SolarWinds/InformationService/v3/Json/Create/Orion.Nodes>)
+
+```
+solarwinds_functions::config::addurl: ""
+```
+
+The SNMP V2 Community String that you want to use
+
+```
+solarwinds_functions::config::community: ""
+```
 
 The SNMP V3 Details that you want to use
 
-    solarwinds_functions::config::snmpv3username: ""
-    solarwinds_functions::config::snmpv3privmethod: ""
-    solarwinds_functions::config::snmpv3privkey: ""
-    solarwinds_functions::config::snmpv3authmethod: ""
-    solarwinds_functions::config::snmpv3authkey: ""
-
+```
+solarwinds_functions::config::snmpv3username: ""
+solarwinds_functions::config::snmpv3privmethod: ""
+solarwinds_functions::config::snmpv3privkey: ""
+solarwinds_functions::config::snmpv3authmethod: ""
+solarwinds_functions::config::snmpv3authkey: ""
+```
 
 Add the IDs of your pollers so it can pick one at random to spread the load. This should be a comma separated single string (ex: "2,3,4,5")
-    
-    solarwinds_functions::config::pollers: ""
+
+```
+solarwinds_functions::config::pollers: ""
+```
 
 Finally, in a profile manifest add the following function:
-    
-    add_node_to_solarwinds()
+
+```
+add_node_to_solarwinds()
+```
--- lib/puppet/parser/functions/add_node_to_solarwinds.rb
+++ lib/puppet/parser/functions/add_node_to_solarwinds.rb
@@ -1,75 +1,89 @@
-require "uri"
-require "yaml"
-require "json"
-require "net/http"
+require 'uri'
+require 'yaml'
+require 'json'
+require 'net/http'
 
 # This is a custom function to add nodes to Solarwinds that aren't already there.
-# This function uses only SNMPV3 to connect to solarwinds.
+# This function can use either SNMP Version 2 or 3 to connect to solarwinds.
+# It assumes that Windows hosts are V2 and everything else is V3
 
 module Puppet::Parser::Functions
-  newfunction(:add_node_to_solarwinds) do |args|
-    config = {}    
+  newfunction(:add_node_to_solarwinds) do |_args|
+    config = {}
 
-    config["username"]  = call_function('hiera',['solarwinds_functions::config::username'])
-    config["password"]  = call_function('hiera',['solarwinds_functions::config::password'])
-    config["queryurl"]  = call_function('hiera',['solarwinds_functions::config::queryurl'])
-    config["addurl"]    = call_function('hiera',['solarwinds_functions::config::addurl'])
-    config["snmpv3username"] = call_function('hiera',['solarwinds_functions::config::snmpv3username'])
-    config["snmpv3privmethod"] = call_function('hiera',['solarwinds_functions::config::snmpv3privmethod'])
-    config["snmpv3privkey"] = call_function('hiera',['solarwinds_functions::config::snmpv3privkey'])
-    config["snmpv3authmethod"] = call_function('hiera',['solarwinds_functions::config::snmpv3authmethod'])
-    config["snmpv3authkey"] = call_function('hiera',['solarwinds_functions::config::snmpv3authkey'])
-    config["pollers"]   = call_function('hiera',['solarwinds_functions::config::pollers'])
-    config["engineid"]  = config["pollers"].split(",").sample
-    config["nodename"]  = lookupvar('fqdn')
-    config["ipaddr"]    = lookupvar('ipaddress')
+    config['username']          = call_function('hiera', ['solarwinds_functions::config::username'])
+    config['password']          = call_function('hiera', ['solarwinds_functions::config::password'])
+    config['queryurl']          = call_function('hiera', ['solarwinds_functions::config::queryurl'])
+    config['addurl']            = call_function('hiera', ['solarwinds_functions::config::addurl'])
+    config['community']         = call_function('hiera', ['solarwinds_functions::config::community'])
+    config['snmpv3username']    = call_function('hiera', ['solarwinds_functions::config::snmpv3username'])
+    config['snmpv3privmethod']  = call_function('hiera', ['solarwinds_functions::config::snmpv3privmethod'])
+    config['snmpv3privkey']     = call_function('hiera', ['solarwinds_functions::config::snmpv3privkey'])
+    config['snmpv3authmethod']  = call_function('hiera', ['solarwinds_functions::config::snmpv3authmethod'])
+    config['snmpv3authkey']     = call_function('hiera', ['solarwinds_functions::config::snmpv3authkey'])
+    config['pollers']           = call_function('hiera', ['solarwinds_functions::config::pollers'])
+    config['engineid']          = config['pollers'].split(',').sample
+    config['nodename']          = lookupvar('fqdn')
+    config['ipaddr']            = lookupvar('ipaddress')
+    config['osname']            = lookupvar('osfamily')
+
+    if config['osname'] == 'Windows'
+      # for now, we will assume that we are stuck with V2 for Windows
+      config['SNMPVersion'] = 2
+    else
+      config['SNMPVersion'] = 3
+    end
 
     response = checkstatus(config)
 
-    if response == '{"results":[]}'
-      addhost(config)
-    end
-
+    addhost(config) if response == '{"results":[]}'
   end
 end
 
 # Reach out to the Solarwinds (Orion) API and ask if the host is present
 def checkstatus(config)
-
-  uri = URI.parse("#{config["queryurl"]}")
-  query = {"query" => "SELECT NodeID FROM Orion.Nodes WHERE NodeName=@name", "parameters" => {"name" => "#{config["nodename"]}"}}
+  uri = URI.parse((config['queryurl']).to_s)
+  query = { 'query' => 'SELECT NodeID FROM Orion.Nodes WHERE NodeName=@name', 'parameters' => { 'name' => (config['nodename']).to_s } }
 
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  
-  request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
+
+  request = Net::HTTP::Post.new(uri.request_uri, initheader = { 'Content-Type' => 'application/json' })
   request.body = query.to_json
-  request.basic_auth("#{config["username"]}", "#{config["password"]}")
+  request.basic_auth((config['username']).to_s, (config['password']).to_s)
 
   response = http.request(request)
 
-  return response.body.to_s
+  response.body.to_s
 end
 
 # If the host was not present in the checkstatus() method, then we add it
 def addhost(config)
-
-  uri = URI.parse("#{config["addurl"]}")
-  node = { "EntityType" => "Orion.Nodes", "IPAddress" => "#{config["ipaddr"]}",
-    "Caption"=> "#{config["nodename"]}", "DynamicIP" => "False", "EngineID" => "#{config["engineid"]}", 
-    "Status" => 1, "UnManaged" => "False", "Allow64BitCounters" => "True", 
-    "SysObjectID" => "", "MachineType" => "", "VendorIcon" => "", 
-    "ObjectSubType" => "SNMP", "SNMPVersion" => 3, "SNMPV3Username" => "#{config["snmpv3username"]}", "SNMPV3Context" => "", "SNMPV3PrivKey" => "#{config["snmpv3privkey"]}", "SNMPV3PrivKeyIsPwd" => 1, "SNMPV3PrivMethod" => "#{config["snmpv3privmethod"]}", "SNMPV3AuthMethod" => "#{config["snmpv3authmethod"]}", "SNMPV3AuthKey" => "#{config["snmpv3authkey"]}", "SNMPV3AuthKeyIsPwd" => 1,
-  }
+  uri = URI.parse((config['addurl']).to_s)
+  if config['SNMPVersion'] == 2
+    # do V2
+    node = { 'EntityType' => 'Orion.Nodes', 'IPAddress' => (config['ipaddr']).to_s,
+             'Caption' => (config['nodename']).to_s, 'DynamicIP' => 'False', 'EngineID' => (config['engineid']).to_s,
+             'Status' => 1, 'UnManaged' => 'False', 'Allow64BitCounters' => 'True',
+             'SysObjectID' => '', 'MachineType' => '', 'VendorIcon' => '',
+             'ObjectSubType' => 'SNMP', 'SNMPVersion' => config['SNMPVersion'], 'Community' => (config['community']).to_s }
+  else
+    # do V3
+    node = { 'EntityType' => 'Orion.Nodes', 'IPAddress' => (config['ipaddr']).to_s,
+             'Caption' => (config['nodename']).to_s, 'DynamicIP' => 'False', 'EngineID' => (config['engineid']).to_s,
+             'Status' => 1, 'UnManaged' => 'False', 'Allow64BitCounters' => 'True',
+             'SysObjectID' => '', 'MachineType' => '', 'VendorIcon' => '',
+             'ObjectSubType' => 'SNMP', 'SNMPVersion' => config['SNMPVersion'], 'SNMPV3Username' => (config['snmpv3username']).to_s, 'SNMPV3Context' => '', 'SNMPV3PrivKey' => (config['snmpv3privkey']).to_s, 'SNMPV3PrivKeyIsPwd' => 1, 'SNMPV3PrivMethod' => (config['snmpv3privmethod']).to_s, 'SNMPV3AuthMethod' => (config['snmpv3authmethod']).to_s, 'SNMPV3AuthKey' => (config['snmpv3authkey']).to_s, 'SNMPV3AuthKeyIsPwd' => 1 }
+  end
 
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-  request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
+  request = Net::HTTP::Post.new(uri.request_uri, initheader = { 'Content-Type' => 'application/json' })
   request.body = node.to_json
-  request.basic_auth("#{config["username"]}", "#{config["password"]}")
+  request.basic_auth((config['username']).to_s, (config['password']).to_s)
 
   response = http.request(request)
 end
